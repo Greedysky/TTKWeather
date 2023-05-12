cb27643794b17e542f2496e7c48d32ccc3a9b59b
Greedysky <greedysky@163.com>
2023-05-12 22:38:55 +0800

    refactor: Optimize global command line module[396554]

--- TTKModule/TTKCore/weatherCoreKits/weatherobject.h
+++ TTKModule/TTKCore/weatherCoreKits/weatherobject.h
@@ -30,10 +30,17 @@
 #define COM_FILE                TTK_STRCAT(TTK_DOT, COM_FILE_SUFFIX)
 
 
+#define SERVICE_NAME            "TTKService"
 #define APP_NAME                "TTKWeather"
 #define APP_DOT_NAME            TTK_STRCAT(APP_NAME, TTK_DOT)
 #define APP_COME_NAME           TTK_STRCAT(APP_NAME, COM_FILE)
-#define APP_EXE_NAME            TTK_STRCAT(APP_NAME, EXE_FILE)
+#ifdef Q_OS_WIN
+#  define APP_EXE_NAME          TTK_STRCAT(APP_NAME, EXE_FILE)
+#  define SERVICE_EXE_NAME      TTK_STRCAT(SERVICE_NAME, EXE_FILE)
+#else
+#  define APP_EXE_NAME          APP_NAME
+#  define SERVICE_EXE_NAME      SERVICE_NAME
+#endif
 
 
 /*! @brief The namespace of the application object.
