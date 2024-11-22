911d98be6d4a16e46cf5bd5834e40bc84b616dec
Greedysky <greedysky@163.com>
2024-11-22 16:01:17 +0800

    refactor: Optimize common library module[089213]

--- TTKRun/TTKApp/ttkrunmain.cpp
+++ TTKRun/TTKApp/ttkrunmain.cpp
@@ -2,7 +2,7 @@
 #include "weatherapplication.h"
 #include "weatherobject.h"
 #include "ttkdumper.h"
-#include "ttkglobalhelper.h"
+#include "ttkglobalwrapper.h"
 #include "ttkplatformsystem.h"
 
 #ifdef Q_OS_UNIX
